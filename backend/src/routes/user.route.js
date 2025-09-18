ee12b53789401adde5c87b20a9e42a4ffdca9711
moran <sharon.ky.moran@gmail.com>
2025-09-18 00:43:59 -0400

    localhost:PORT/api/users working with test response

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
+// localhost:PORT/api/users is showing correct response
 router.get("/", (req, res) => {
   res.send("User route with GET Method");
 });
