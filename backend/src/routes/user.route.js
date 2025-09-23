5d9b377fd41555b9d6adbb5cc62005d88e1ade8e
moran <sharon.ky.moran@gmail.com>
2025-09-23 15:02:12 -0400

    req.auth depreciated, use req.auth()

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -5,13 +5,14 @@ const router = Router();
 
 // localhost:PORT/api/users is showing correct response
 router.get("/", (req, res) => {
-  req.auth.userId;
+  req.auth().userId;
   res.send("User route with GET Method");
 });
 
 // a user shouldnt be able to like some media without being authenticated
 // so the route needs to be protected
 // router.get("/like", protectRoute, (req, res) => {
+/* Clerk - DEPRECATION WARNING: "req.auth" is deprecated and will be removed in the next major release. Use `req.auth()` as a function instead. */
 //   req.auth.userId;
 //   res.send("user route for media likes get method");
 // });
