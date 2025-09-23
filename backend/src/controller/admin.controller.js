abbf16b74b21418c2ce65a510ee1da1e13a0bb7d
moran <sharon.ky.moran@gmail.com>
2025-09-23 15:57:47 -0400

    setup to allow admin to create media

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,3 +1,6 @@
-export const getAdmin = (req, res) => {
-  res.send("Admin route with GET method");
-};
+// test simple connection to admin route and controller
+// export const getAdmin = (req, res) => {
+//   res.send("Admin route with GET method");
+// };
+
+// admin should be able to create media (todo: upload image, audiofile, media info)
