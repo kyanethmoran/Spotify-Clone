dac08fb5ddba10cec69aab8c099268b69a60ce11
moran <sharon.ky.moran@gmail.com>
2025-09-23 15:57:03 -0400

    setup to let admin create media

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { getAdmin } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", getAdmin);
+// test that getAdmin from controller is working
+// router.get("/", getAdmin);
+
+// uses protected route and requires admin status, only admin can access
+router.get("/", protectRoute, requireAdmin, createMedia);
 
 export default router;
