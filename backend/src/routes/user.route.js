77441977ad3a7ce31c15af64b2ae2da7d48bbe3c
moran <sharon.ky.moran@gmail.com>
2025-09-23 14:55:17 -0400

    protected route example

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 // localhost:PORT/api/users is showing correct response
 router.get("/", (req, res) => {
+  req.auth.userId;
   res.send("User route with GET Method");
 });
+
+// a user shouldnt be able to like some media without being authenticated
+// so the route needs to be protected
+// router.get("/like", protectRoute, (req, res) => {
+//   req.auth.userId;
+//   res.send("user route for media likes get method");
+// });
 
 export default router;
