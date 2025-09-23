f3fa0e1a8a78c7dbcb4bcd3fa7a71f6b8cc67afe
moran <sharon.ky.moran@gmail.com>
2025-09-23 14:57:34 -0400

    auth middleware layer check for admin on routes

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import { clerkClient } from "@clerk/express";
+
+// make sure that user is authenticated
+export const protectRoute = async (req, res, next) => {
+  if (!req.auth.userId) {
+    return req
+      .status(401)
+      .json({ message: "Unauthorized - you must be logged in" });
+  }
+
+  next();
+};
+
+// ensure that only admins can access the route, not just regular users
+export const requireAdmin = async (req, res, next) => {
+  try {
+    const currentUser = await clerkClient.users.getUser(req.auth.userId);
+    const isAdmin =
+      process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress;
+
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ messsage: "Unauthorized - you must be an admin" });
+    }
+
+    next();
+  } catch (error) {
+    console.log("error occured when trying to validate for admin rights");
+  }
+};
