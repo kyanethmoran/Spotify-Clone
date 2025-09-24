1dee2c6497d72714eeac225089d158c4d794ec98
moran <sharon.ky.moran@gmail.com>
2025-09-24 01:29:08 -0400

    error handling in catch for controllers

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 
-export const authCall = async (req, res) => {
+export const authCall = async (req, res, next) => {
   try {
     const { id, firstName, lastName, imageUrl } = req.body;
 
@@ -19,6 +19,6 @@ export const authCall = async (req, res) => {
     res.status(200).json({ success: true });
   } catch (error) {
     console.log("Error in auth callback", error);
-    res.status(500).json({ message: "Internal server error", error });
+    next(error);
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -50,6 +50,18 @@ app.use("/api/media", mediaRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
+// error handler
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal server error"
+          : err.message,
+    });
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
   connectDB();
