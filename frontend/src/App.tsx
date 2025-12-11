35d12e43b015f5cc39a1d587a2c32689f6dc1e50
moran <sharon.ky.moran@gmail.com>
2025-12-10 20:14:41 -0500

    comments for later about auth provider for sending requests so clerk is aware of authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,11 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage.tsx";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
+// import { axiosInstance } from "./lib/axios.ts";
 
 function App() {
+  // Tell Clerk we are authenticated when sending requests (token from provider)
+
   return (
     <>
       <Routes>
