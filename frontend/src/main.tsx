3169ed72ac681ddd423ec62558103b9b19f7f012
moran <sharon.ky.moran@gmail.com>
2025-12-12 13:09:35 -0500

    Auth provider now authenticating whenever a page refreshes, loading symbol is displayed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./providers/AuthProvider.tsx";
 
 // Clerk: Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,9 +17,11 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </ClerkProvider>
   </StrictMode>
 );
