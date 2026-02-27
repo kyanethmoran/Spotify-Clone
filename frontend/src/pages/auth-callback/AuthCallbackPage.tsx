b97840eb3f249c4cf7a2248c3333a0b7f3ea665e
moran <sharon.ky.moran@gmail.com>
2026-02-27 18:58:29 -0500

    Card Created to show status and redirect update

--- frontend/src/pages/auth-callback/AuthCallbackPage.tsx
+++ frontend/src/pages/auth-callback/AuthCallbackPage.tsx
@@ -1,11 +1,16 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Loader } from "lucide-react";
 import React from "react";
 
 const AuthCallbackPage = () => {
   return (
     <div className="h-screen w-full bg-black flex items-center justify-center">
-      <Card>
-        <CardContent></CardContent>
+      <Card className="w-[90%] max-w-md bg-zinc-900 border-zinc-800">
+        <CardContent className="flex flex-col items-center gap-4 pt-6">
+          <Loader className="size-6 text-orange-500 animate-spin" />
+          <h3 className="text-zinc-400 text-xl font-bold">Logging you in</h3>
+          <p className="text-zinc-400 text-sm animate-pulse">Redirecting...</p>
+        </CardContent>
       </Card>
     </div>
   );
