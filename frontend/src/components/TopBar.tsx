0e6b7862601a6c11a82bee3b439c8ac40e1ef0ad
moran <sharon.ky.moran@gmail.com>
2026-02-27 17:13:03 -0500

    implemented clerk signout button

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import { SignedOut } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
@@ -16,6 +16,10 @@ const TopBar = () => {
             Admin Dashboard
           </Link>
         )}
+
+        <SignedIn>
+          <SignOutButton />
+        </SignedIn>
 
         <SignedOut>
           <SignInOAuthButtons />
