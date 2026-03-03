20d95e07139beb6b2b141088311c4b7f122de365
moran <sharon.ky.moran@gmail.com>
2026-03-03 16:30:48 -0500

    correct base url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // url to our backend
-  baseURL: "https://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
 });
