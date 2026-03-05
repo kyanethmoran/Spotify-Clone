abe1f130b763b515b890e90497085f98514d1693
moran <sharon.ky.moran@gmail.com>
2026-03-05 13:06:21 -0500

    panel group created, with 3 main panels and adjustable handles

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,9 +1,39 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
+  const isMoble = false;
+
   return (
     <div className="h-screen bg-black text-white flex flex-col">
-      {/* implement resizeable panels here for mmain layout */}
+      <ResizablePanelGroup direction="horizontal">
+        {/* left side component */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMoble ? 0 : 10}
+          maxSize={30}
+        >
+          left side component
+        </ResizablePanel>
+        <ResizableHandle />
+        {/* main component */}
+        <ResizablePanel maxSize={80}>
+          <Outlet />
+        </ResizablePanel>
+        <ResizableHandle />
+        {/* right side component */}
+        <ResizablePanel
+          defaultSize={20}
+          minSize={isMoble ? 0 : 10}
+          maxSize={30}
+        >
+          right side component
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
