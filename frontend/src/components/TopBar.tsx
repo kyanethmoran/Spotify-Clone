ddddbc6064f7106e979afa4de58fe3e8e9e6290e
moran <sharon.ky.moran@gmail.com>
2026-03-23 15:53:31 -0400

    round top bar corners

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -7,7 +7,7 @@ const TopBar = () => {
   const isAdmin = false;
 
   return (
-    <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
+    <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10 rounded-md">
       <div className="flex gap-2 items-center">Listener</div>
       <div className="flex items-center gap-4">
         {isAdmin && (
